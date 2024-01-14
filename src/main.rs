32e85bfe984e530e72ca659e0b354f7018de4f83
SophieLeichtle <sophie.leichtle@gmail.com>
2024-01-14 16:15:42 +0100

    implemented vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 #![no_std]
 #![no_main] 
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-     loop {}
+    println!("Hello World{}", "!");
+    panic!("Some panic Message");
+    loop {}
 }
