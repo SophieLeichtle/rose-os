b5b91d97eec2d1296d433a8e97379604e579e0ed
SophieLeichtle <sophie.leichtle@gmail.com>
2024-01-15 15:36:44 +0100

    testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,9 +1,7 @@
 #![no_std]
-#![no_main] 
-
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rose_os::test_runner)]
-
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
@@ -11,23 +9,17 @@ use rose_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-    
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rose_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
 }
