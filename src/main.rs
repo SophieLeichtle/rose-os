54bc67f38f64b3209fa3305da4a0b66934d7bab9
SophieLeichtle <sophie.leichtle@gmail.com>
2024-01-18 15:18:31 +0100

    hardware interrupts

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("No Crashes!");
-    loop {}
+    rose_os::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    rose_os::hlt_loop();
 }
 
 #[cfg(test)]
